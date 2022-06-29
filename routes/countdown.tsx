757ee12a856163acfeb1096debaa1b36c1aed8d6
Hadi Faraz <hadifaraz52@protonmail.com>
2022-06-29 14:08:04 -0600

    Add project

---
+++ routes/countdown.tsx
@@ -0,0 +1,15 @@
+/** @jsx h */
+import { h } from "preact";
+import Countdown from "../islands/Countdown.tsx";
+
+export default function Page() {
+    const date = new Date();
+
+    date.setHours(date.getHours() + 1);
+
+    return (
+        <p>
+            The big event is happening <Countdown target={date.toISOString()} />
+        </p>
+    );
+}
---
+++ routes/github/[username].tsx
@@ -0,0 +1,37 @@
+/** @jsx h */
+import { h } from "preact";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { tw } from "@twind";
+
+interface User {
+    login: string,
+    name: string,
+    avatar_url: string,
+}
+
+export const handler: Handlers<User | null> = {
+    async GET(reqq, ctx) {
+        const { username } = ctx.params;
+        const resp = await fetch(`https://api.github.com/users/${username}`);
+
+        if (resp.status == "404") return ctx.render(null);
+
+        const user = await resp.json();
+
+        console.log(reqq);
+
+        return ctx.render(user);
+    },
+};
+
+export default function Page({ data }: PageProps<User | null>) {
+    if (!data) return <h1>User not found</h1>;
+
+    return (
+        <div class={tw`p-4 mx-auto max-w-screen-md`}>
+            <img alt="Profile Picture" src={data.avatar_url} width={64} height={64} />
+            <h1>{data.name}</h1>
+            <p>{data.login}</p>
+        </div>
+    )
+}
